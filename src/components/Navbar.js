2c9aff6eba54778e4b4ed37f5630255ab86b59c7
Cristian <camilo.santamaria322@gmail.com>
2021-06-30 18:19:39 -0500

    Se agrega url que son visibles solo si se loggea

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,66 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
-import useAuth from '../auth/useAuth'
+import useAuth from "../auth/useAuth";
 
 export default function Navbar() {
-    const auth = useAuth();
+  const auth = useAuth();
 
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink exact to="/" activeClassName="active" >Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about"  activeClassName="active">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/categories" activeClassName="active">Categories</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login" activeClassName="active">Login</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" activeClassName="active">Register</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/dashboard" activeClassName="active">Dashboard</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/payments" activeClassName="active">Payments</NavLink>
-                </li>
-                <li>
-                    <button onClick={auth.logout}>Logout</button>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="active">
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories" activeClassName="active">
+            Categories
+          </NavLink>
+        </li>
+         {!auth.isLogged() && (
+        <>
+        <li>
+          <NavLink to="/login" activeClassName="active">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register" activeClassName="active">
+            Register
+          </NavLink>
+        </li>
+        </>
+         )}
+        {auth.isLogged() && (
+          <>
+            <li>
+              <NavLink to="/dashboard" activeClassName="active">
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/payments" activeClassName="active">
+                Payments
+              </NavLink>
+            </li>
+
+            <li>
+              <button onClick={auth.logout}>Logout</button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
